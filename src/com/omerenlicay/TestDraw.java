207a2d7ba5e8147f1a2c31184bf77e94b502bfb8
Ömer ENLİÇAY <omerenlicay@gmail.com>
2016-10-22 16:18:32 +0300

    Java Paint Uygulaması

---
+++ src/com/omerenlicay/TestDraw.java
@@ -0,0 +1,21 @@
+package com.omerenlicay;
+/**
+ * Grafik Programlama Odevi
+ * Name: Omer Enlicay
+ * Date: Ekim 2016
+ * 
+ * Acıklama:
+ * Bu program iki adet panelden oluşmaktadır.
+ * Üst kısımdaki panelde widgetler (Button, checkBox) bulunmaktadır.
+ * Alt kısımdaki panel cizim panelidir. Secilen widget dogrulutusunda cizim yazpılabilr.
+ * 
+ * LinkedList kullanılarak geri alma ileri alma ve tamamını temizleme gibi özellikler kullanılmıştır.
+ * 
+ */
+public class TestDraw
+{
+    public static void main( String args[] )
+    {
+        DrawFrame paintGui = new DrawFrame(); //initalize DrawFrame object called paintGui     
+    } // end main
+} // end class TestDraw
